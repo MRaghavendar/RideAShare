2fbc881432cb4e3365c5c322db94ac307ab162af
Yeshwanthreddy2019 <yeshwanthusa@gmail.com>
2019-10-04 19:04:42 -0500

    Completed the layout of request a ride and Search results

---
+++ app/src/main/java/com/example/rideashare/Request_A_Ride.java
@@ -0,0 +1,14 @@
+package com.example.rideashare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Request_A_Ride extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request__a__ride);
+    }
+}
