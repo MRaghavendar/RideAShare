4ecea7bee8aa8f2acd5de4211865a00b9819a76e
Raghavendra <raghavendarm10@gmail.com>
2019-10-04 21:35:53 -0500

    updated button functions and added new buttons by Raghavendra

--- app/src/main/java/com/example/rideashare/Details_of_ride.java
+++ app/src/main/java/com/example/rideashare/Details_of_ride.java
@@ -2,7 +2,9 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Details_of_ride extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Details_of_ride extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_of_ride);
+    }
+
+    public void close_fun(View v){
+        Intent intent1= new Intent(this,Search_result.class);
+        startActivity(intent1);
     }
 }
