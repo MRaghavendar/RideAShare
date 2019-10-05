8311d56fc953241a6b57453631e59612d667ebcb
Raghavendra <raghavendarm10@gmail.com>
2019-10-04 19:26:30 -0500

    Completed search results layout by Raghavendra

---
+++ app/src/main/java/com/example/rideashare/Details_of_ride.java
@@ -0,0 +1,14 @@
+package com.example.rideashare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Details_of_ride extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_of_ride);
+    }
+}
