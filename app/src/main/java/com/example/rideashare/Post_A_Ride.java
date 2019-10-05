fb3aa59dc7e2d214021bef33f90d77a558ae3de4
Nishanthreddy1997 <46684557+Nishanthreddy1997@users.noreply.github.com>
2019-10-04 20:38:04 -0500

    Added functions to post,signup buttons

--- app/src/main/java/com/example/rideashare/Post_A_Ride.java
+++ app/src/main/java/com/example/rideashare/Post_A_Ride.java
@@ -2,7 +2,9 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Post_A_Ride extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Post_A_Ride extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post__a__ride);
+    }
+    public void backToHome(View v){
+        Intent b1= new Intent(this,WelcomePage.class);
+        startActivity(b1);
     }
 }
--- app/src/main/java/com/example/rideashare/Sign_up.java
+++ app/src/main/java/com/example/rideashare/Sign_up.java
@@ -2,7 +2,9 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sign_up extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+    public void signup(View v){
+        Intent intent1= new Intent(this,MainActivity.class);
+        startActivity(intent1);
     }
 }
