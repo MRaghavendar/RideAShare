f4aec460ef680a57bd4c8d06d5e04cccbe0b4ca7
milindar <milindarreddy.1996@gmail.com>
2019-10-04 19:45:43 -0500

    added fuctions for buttons(Login , SignUp) by Millindar

--- app/src/main/java/com/example/rideashare/MainActivity.java
+++ app/src/main/java/com/example/rideashare/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Login_Fun(View v){
+        Intent b1= new Intent(this,WelcomePage.class);
+        startActivity(b1);
+    }
+    public void Signup_fun(View v){
+        Intent b1= new Intent(this,Sign_up.class);
+        startActivity(b1);
     }
 }
--- app/src/main/java/com/example/rideashare/WelcomePage.java
+++ app/src/main/java/com/example/rideashare/WelcomePage.java
@@ -2,7 +2,9 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomePage extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class WelcomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+    }
+    public void PostRide(View v){
+        Intent b1= new Intent(this,Post_A_Ride.class);
+        startActivity(b1);
     }
 }
