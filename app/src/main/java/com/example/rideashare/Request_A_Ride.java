9d5de3f50f133243e7cca01f3c996f23e60dde05
Yeshwanthreddy2019 <yeshwanthusa@gmail.com>
2019-10-04 21:02:23 -0500

    Added button functions by yeshwanth

--- app/src/main/java/com/example/rideashare/Request_A_Ride.java
+++ app/src/main/java/com/example/rideashare/Request_A_Ride.java
@@ -2,7 +2,9 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Request_A_Ride extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Request_A_Ride extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request__a__ride);
+    }
+
+    public void Search_fun(View v){
+        Intent intent1= new Intent(this,Search_result.class);
+        startActivity(intent1);
     }
 }
--- app/src/main/java/com/example/rideashare/Search_result.java
+++ app/src/main/java/com/example/rideashare/Search_result.java
@@ -2,7 +2,9 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Search_result extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Search_result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+    }
+    public void details(View v){
+        Intent intent1= new Intent(this,Details_of_ride.class);
+        startActivity(intent1);
     }
 }
