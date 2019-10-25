aacccf257f47cdb862923f77bb20ee465059f672
milindar <milindarreddy.1996@gmail.com>
2019-10-25 14:39:19 -0500

    updated Sign_up.java file with sharedpreferences

--- app/src/main/java/com/example/rideashare/MainActivity.java
+++ app/src/main/java/com/example/rideashare/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Login_Fun(View v){
-        Intent intent1= new Intent(this,WelcomePage.class);
+
+    public void login_Fun(View V) {
+//        EditText etName = findViewById(R.id.uNameET);
+//        EditText etPassword = findViewById(R.id.pwdET);
+//        SharedPreferences preferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+//        String user = preferences.getString("username", "");
+//        String password = preferences.getString("password", "");
+//        if (!user.equals("") && !password.equals("")) {
+//            if (user.equalsIgnoreCase(etName.getText().toString()) && password.equalsIgnoreCase(etPassword.getText().toString())) {
+                Intent intent1 = new Intent(MainActivity.this, WelcomePage.class);
+                startActivity(intent1);
+//            } else {
+//                Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+//            }
+//        } else {
+//            Toast.makeText(getApplicationContext(), "No Data Found", Toast.LENGTH_SHORT).show();
+//        }
+    }
+
+    public void Signup_fun(View v) {
+        Intent intent1 = new Intent(this, Sign_up.class);
         startActivity(intent1);
     }
-    public void Signup_fun(View v){
-        Intent intent1= new Intent(this,Sign_up.class);
-        startActivity(intent1);
-    }
+
 }
--- app/src/main/java/com/example/rideashare/Sign_up.java
+++ app/src/main/java/com/example/rideashare/Sign_up.java
@@ -2,9 +2,13 @@ package com.example.rideashare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Sign_up extends AppCompatActivity {
 
@@ -12,9 +16,23 @@ public class Sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
-    public void signup(View v){
-        Intent intent1= new Intent(this,MainActivity.class);
+
+    public void signup(View v) {
+        EditText usernameET = findViewById(R.id.lNameEdit);
+        EditText passwordET = findViewById(R.id.passwordEdit);
+        EditText email = findViewById(R.id.emailEdit);
+        SharedPreferences preferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        String newUser = usernameET.getText().toString();
+        String newPass = passwordET.getText().toString();
+        String newEmail = email.getText().toString();
+        editor.putString("username", newEmail);
+        editor.putString("password", newPass);
+        editor.commit();
+        Toast.makeText(getApplicationContext(), "successful", Toast.LENGTH_SHORT).show();
+        Intent intent1 = new Intent(this, MainActivity.class);
         startActivity(intent1);
     }
 }
