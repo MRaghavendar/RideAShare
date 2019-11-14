5731b0af0eae201718e6826ccea6b0aaa1c8a737
milindar <milindarreddy.1996@gmail.com>
2019-11-14 13:17:37 -0600

    Updated Gradle Scripts for firebase by millindar

--- app/src/main/java/com/example/rideashare/MainActivity.java
+++ app/src/main/java/com/example/rideashare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rideashare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -7,36 +8,110 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
+    EditText emailId, password;
+    Button btnSignUp;
+    Button SignInBtn;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void login_Fun(View V) {
-        EditText etName = findViewById(R.id.uNameET);
-        EditText etPassword = findViewById(R.id.pwdET);
-        SharedPreferences preferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
-        String user = preferences.getString("username", "");
-        String password = preferences.getString("password", "");
-        if (!user.equals("") && !password.equals("")) {
-            if (user.equalsIgnoreCase(etName.getText().toString()) && password.equalsIgnoreCase(etPassword.getText().toString())) {
-                Intent intent1 = new Intent(MainActivity.this, WelcomePage.class);
-                startActivity(intent1);
-            } else {
-                Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Toast.makeText(getApplicationContext(), "No Data Found", Toast.LENGTH_SHORT).show();
-        }
-    }
+//        mFirebaseAuth = FirebaseAuth.getInstance();
+//        emailId = findViewById(R.id.uNameET);
+//        password = findViewById(R.id.pwdET);
+//        btnSignUp = findViewById(R.id.signupBTN);
+//        SignInBtn = findViewById(R.id.log_inBTN);
+//
+//        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+//                if( mFirebaseUser != null ){
+//                    Toast.makeText(MainActivity.this,"You are logged in",Toast.LENGTH_SHORT).show();
+//                    Intent i = new Intent(MainActivity.this, WelcomePage.class);
+//                    startActivity(i);
+//                }
+//                else{
+//                    Toast.makeText(MainActivity.this,"Please Login",Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        };
+//
+//        SignInBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String email = emailId.getText().toString();
+//                String pwd = password.getText().toString();
+//                if(email.isEmpty()){
+//                    emailId.setError("Please enter email id");
+//                    emailId.requestFocus();
+//                }
+//                else  if(pwd.isEmpty()){
+//                    password.setError("Please enter your password");
+//                    password.requestFocus();
+//                }
+//                else  if(email.isEmpty() && pwd.isEmpty()){
+//                    Toast.makeText(MainActivity.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+//                }
+//                else  if(!(email.isEmpty() && pwd.isEmpty())){
+//                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if(!task.isSuccessful()){
+//                                Toast.makeText(MainActivity.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+//                            }
+//                            else{
+//                                Intent intToHome = new Intent(MainActivity.this,WelcomePage.class);
+//                                startActivity(intToHome);
+//                            }
+//                        }
+//                    });
+//                }
+//                else{
+//                    Toast.makeText(MainActivity.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+//
+//                }
+//
+//            }
+//        });
+//
+//
+//    }
 
+//    public void login_Fun(View V) {
+//        EditText etName = findViewById(R.id.uNameET);
+//        EditText etPassword = findViewById(R.id.pwdET);
+//        SharedPreferences preferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+//        String user = preferences.getString("username", "");
+//        String password = preferences.getString("password", "");
+//        if (!user.equals("") && !password.equals("")) {
+//            if (user.equalsIgnoreCase(etName.getText().toString()) && password.equalsIgnoreCase(etPassword.getText().toString())) {
+//                Intent intent1 = new Intent(MainActivity.this, WelcomePage.class);
+//                startActivity(intent1);
+//            } else {
+//                Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+//            }
+//        } else {
+//            Toast.makeText(getApplicationContext(), "No Data Found", Toast.LENGTH_SHORT).show();
+//        }
+//
+    }
     public void Signup_fun(View v) {
         Intent intent1 = new Intent(this, Sign_up.class);
         startActivity(intent1);
