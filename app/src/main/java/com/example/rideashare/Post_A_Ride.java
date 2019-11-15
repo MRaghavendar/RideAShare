db1fbda987f1e620d851e8f9eea94852b77e3b88
milindar <milindarreddy.1996@gmail.com>
2019-11-15 13:25:42 -0600

    Updated error message for empty fields by millindar
    source: https://www.youtube.com/watch?v=V0ZrnL-i77Q

--- app/src/main/java/com/example/rideashare/Post_A_Ride.java
+++ app/src/main/java/com/example/rideashare/Post_A_Ride.java
@@ -18,25 +18,25 @@ public class Post_A_Ride extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post__a__ride);
     }
-    public void backToHome(View v){
-        EditText startET = findViewById(R.id.startingEdit);
-        EditText DesET = findViewById(R.id.destinationEdit);
-        EditText seatsET = findViewById(R.id.seatsEdit);
-        EditText locET = findViewById(R.id.locationET);
-        String s = seatsET.getText().toString();
-        EditText timeET = findViewById(R.id.dateEdit);
-        EditText costET = findViewById(R.id.costEdit);
-        SharedPreferences preferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("Starting Point",startET.getText().toString());
-        editor.putString("Destination",DesET.getText().toString());
-        editor.putString("Seats",s);
-        editor.putString("Timing",timeET.getText().toString());
-        editor.putString("Cost",costET.getText().toString());
-        editor.putString("Address", locET.getText().toString());
-        editor.commit();
-        Toast.makeText(getApplicationContext(), "Posted successful", Toast.LENGTH_SHORT).show();
-        Intent b1= new Intent(this,WelcomePage.class);
-        startActivity(b1);
-    }
+//    public void backToHome(View v){
+//        EditText startET = findViewById(R.id.startingEdit);
+//        EditText DesET = findViewById(R.id.destinationEdit);
+//        EditText seatsET = findViewById(R.id.seatsEdit);
+//        EditText locET = findViewById(R.id.locationET);
+//        String s = seatsET.getText().toString();
+//        EditText timeET = findViewById(R.id.dateEdit);
+//        EditText costET = findViewById(R.id.costEdit);
+//        SharedPreferences preferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = preferences.edit();
+//        editor.putString("Starting Point",startET.getText().toString());
+//        editor.putString("Destination",DesET.getText().toString());
+//        editor.putString("Seats",s);
+//        editor.putString("Timing",timeET.getText().toString());
+//        editor.putString("Cost",costET.getText().toString());
+//        editor.putString("Address", locET.getText().toString());
+//        editor.commit();
+//        Toast.makeText(getApplicationContext(), "Posted successful", Toast.LENGTH_SHORT).show();
+//        Intent b1= new Intent(this,WelcomePage.class);
+//        startActivity(b1);
+//    }
 }
