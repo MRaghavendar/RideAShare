747706e6dcbe20d608944e9384f5230700b47d55
milindar <milindarreddy.1996@gmail.com>
2019-12-08 17:59:28 -0600

    Added a textview to display name on the welcome activity by millindar

--- app/src/main/java/com/example/rideashare/WelcomePage.java
+++ app/src/main/java/com/example/rideashare/WelcomePage.java
@@ -1,19 +1,52 @@
 package com.example.rideashare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class WelcomePage extends AppCompatActivity {
-
+        TextView tv;
+        DatabaseReference reff;
+        FirebaseAuth mFirebaseAuth;
+        String user_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        tv = findViewById(R.id.welcome_nameTV);
+        user_id = mFirebaseAuth.getCurrentUser().getUid();
+        reff = FirebaseDatabase.getInstance().getReference().child("details").child(user_id);
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("first_name").getValue().toString();
+
+                tv.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
+
+
     public void PostRide(View v){
         Intent intent1 = new Intent(this,Post_A_Ride.class);
         startActivity(intent1);
@@ -34,5 +67,10 @@ public class WelcomePage extends AppCompatActivity {
         Intent logout_intent = new Intent(this, Requested.class);
         startActivity(logout_intent);
     }
+    public void Myrides_function(View v){
+        Intent logout_intent = new Intent(this, Myrides.class);
+        startActivity(logout_intent);
+    }
+
 
 }
