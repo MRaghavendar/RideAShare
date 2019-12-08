970cb766e828fa97086f867e258fee008836fb58
milindar <milindarreddy.1996@gmail.com>
2019-12-08 15:09:18 -0600

    Added forgot password activity and .java class by millindar

---
+++ app/src/main/java/com/example/rideashare/Forgot_password.java
@@ -0,0 +1,45 @@
+package com.example.rideashare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Forgot_password extends AppCompatActivity {
+
+    EditText email;
+    Button resetBTn;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        email = findViewById(R.id.emailET_for);
+        resetBTn = findViewById(R.id.resetpassBTN);
+        firebaseAuth = FirebaseAuth.getInstance();
+        resetBTn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.sendPasswordResetEmail(email.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Forgot_password.this, "Password send to your email", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(Forgot_password.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
